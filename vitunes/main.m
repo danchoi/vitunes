51716220f8541d114316b6a6c2f6a8c7736cf7e7
Daniel Choi <dhchoi@gmail.com>
2011-06-29 19:49:15 -0400

    happy with this approach

--- vitunes/main.m
+++ vitunes/main.m
@@ -8,15 +8,36 @@
 #import "iTunes.h"
 #import <Foundation/Foundation.h>
 
+// because Cocoa won't let me put a category directly on iTunesTrack.
+@interface SBObject (ViTunes) 
+- (NSString *)test;
+@end
+@implementation SBObject (ViTunes) 
+- (NSString *)test {
+  NSString *name = [self valueForKey:@"name"];
+  NSString *album = [self valueForKey:@"album"];
+  id databaseID = [self valueForKey:@"databaseID"];
+  NSString *artist = [self valueForKey:@"artist"];
+  id year = [self valueForKey:@"year"];
+  NSString *genre = [self valueForKey:@"genre"];
+  return [NSString stringWithFormat: @"%@ %@ %@ %@ (%@) %@ \n", 
+          databaseID,
+          name,
+          album,
+          artist,
+          year,
+          genre];
+
+};
+@end
 
 // functions
 NSString *format(iTunesTrack *track) {
-
   NSString *name = track.name;
   NSString *album = track.album;
-  NSString *databaseID = track.databaseID;
+  NSInteger databaseID = track.databaseID;
   NSString *artist = track.artist;
-  NSString *year = track.year;
+  NSInteger year = track.year;
   NSString *genre = track.genre;
   return [NSString stringWithFormat: @"%d %@ %@ %@ (%d) %@ \n", 
           databaseID,
@@ -33,7 +54,13 @@ static iTunesPlaylist *music;
 void search(NSString *query) {
     SBElementArray *tracks = [music searchFor:query only:iTunesESrAAll];
     NSLog(@"%@", [tracks class]);    
+
+    //NSArray *strings = [tracks arrayByApplyingSelector:@selector(test)];
+    //NSLog(@"result %@", strings);
+    //return;
     for (iTunesTrack *track in tracks) {
+      //printf("%s\n", [format(track) cStringUsingEncoding: NSUTF8StringEncoding]);
+      //printf("%s\n", [[track test] cStringUsingEncoding: NSUTF8StringEncoding]);
       printf("%s\n", [format(track) cStringUsingEncoding: NSUTF8StringEncoding]);
     }
 }
