875b3699645d2b86e9edde274fa9c413d7360e8e
Daniel Choi <dhchoi@gmail.com>
2011-06-29 19:50:42 -0400

    cleanup ;

--- vitunes/main.m
+++ vitunes/main.m
@@ -8,29 +8,6 @@
 #import "iTunes.h"
 #import <Foundation/Foundation.h>
 
-// because Cocoa won't let me put a category directly on iTunesTrack.
-@interface SBObject (ViTunes) 
-- (NSString *)test;
-@end
-@implementation SBObject (ViTunes) 
-- (NSString *)test {
-  NSString *name = [self valueForKey:@"name"];
-  NSString *album = [self valueForKey:@"album"];
-  id databaseID = [self valueForKey:@"databaseID"];
-  NSString *artist = [self valueForKey:@"artist"];
-  id year = [self valueForKey:@"year"];
-  NSString *genre = [self valueForKey:@"genre"];
-  return [NSString stringWithFormat: @"%@ %@ %@ %@ (%@) %@ \n", 
-          databaseID,
-          name,
-          album,
-          artist,
-          year,
-          genre];
-
-};
-@end
-
 // functions
 NSString *format(iTunesTrack *track) {
   NSString *name = track.name;
@@ -54,13 +31,7 @@ static iTunesPlaylist *music;
 void search(NSString *query) {
     SBElementArray *tracks = [music searchFor:query only:iTunesESrAAll];
     NSLog(@"%@", [tracks class]);    
-
-    //NSArray *strings = [tracks arrayByApplyingSelector:@selector(test)];
-    //NSLog(@"result %@", strings);
-    //return;
     for (iTunesTrack *track in tracks) {
-      //printf("%s\n", [format(track) cStringUsingEncoding: NSUTF8StringEncoding]);
-      //printf("%s\n", [[track test] cStringUsingEncoding: NSUTF8StringEncoding]);
       printf("%s\n", [format(track) cStringUsingEncoding: NSUTF8StringEncoding]);
     }
 }
