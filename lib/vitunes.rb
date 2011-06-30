780c6c8379d96d2be28eb85b5fde26513e387304
Daniel Choi <dhchoi@gmail.com>
2011-06-30 10:11:10 -0400

    command line tool to install vitunes vim plugin

--- lib/vitunes.rb
+++ lib/vitunes.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class ViTunes
-  def self.install_plugin
+  def self.install_vim_plugin
     require 'erb'
     plugin_template = File.read(File.join(File.dirname(__FILE__), 'plugin.erb'))
     vimscript_file = File.join(File.dirname(__FILE__), 'vitunes.vim')
@@ -10,7 +10,8 @@ class ViTunes
     File.open("#{ENV['HOME']}/.vim/plugin/vitunes.vim", "w") {|f| f.write plugin_body}
   end
 end
+
 if __FILE__ == $0
-  ViTunes.install_plugin
+  ViTunes.install_vim_plugin
 end
 
