dd9ea4dec4b23819231b80fb7740c1c0305de82d
Daniel Choi <dhchoi@gmail.com>
2011-06-30 02:10:44 -0400

    add older rubycocoa project for reference

--- vitunes/main.m
+++ vitunes/main.m
@@ -3,106 +3,75 @@
 //  vitunes
 //
 //  Created by Daniel Choi on 6/29/11.
-//  Copyright 2011 Software by Daniel Choi. All rights reserved.
+//  Copyright 2011 by Daniel Choi. All rights reserved.
 //
 #import "iTunes.h"
 #import <Foundation/Foundation.h>
 
-// functions
-NSString *format(iTunesTrack *track) {
-  NSString *name = track.name;
-  NSString *album = track.album;
-  NSInteger databaseID = track.databaseID;
-  NSString *artist = track.artist;
-  NSInteger year = track.year;
-  NSString *genre = track.genre;
-  return [NSString stringWithFormat: @"%d %@ %@ %@ (%d) %@", 
-          databaseID,
-          name,
-          album,
-          artist,
-          year,
-          genre];
-          
-}
-
+static iTunesApplication *iTunes;
 static iTunesPlaylist *music;
 static iTunesSource *library;
 
-NSArray *search(NSString *query) {
-    SBElementArray *tracks = [music searchFor:query only:iTunesESrAAll];
-    NSLog(@"%@", [tracks class]);    
-    return tracks;
+NSString *formatTrackForDisplay(iTunesTrack *track) {
+  return [NSString stringWithFormat: @"%d %@ %@ %@ (%d) %@", 
+          track.databaseID,
+          track.name,
+          track.album,
+          track.artist,
+          track.year,
+          track.genre];
 }
 
-
-void playID(NSNumber *databaseId) {
-     NSArray *matchingTracks = [[music tracks] 
-       filteredArrayUsingPredicate:
-         [NSPredicate predicateWithFormat:@"databaseID == %@", databaseId]];
-     iTunesTrack* t = [matchingTracks objectAtIndex:0];
-     NSLog(@"found track %@", [t name]);
-     [t playOnce:false];
-}
-    
-
-// 
-void play(NSString *query, NSString *index) {
-    printf("play %s %s\n", 
-        [query cStringUsingEncoding: NSUTF8StringEncoding],
-        [index cStringUsingEncoding: NSUTF8StringEncoding]);
-    int idx = [index intValue];
-    NSArray *tracks = search(query);
-    iTunesTrack *t = [tracks objectAtIndex:idx];
-    NSLog(@"%@", [t name]);
-    [t playOnce:true];
+SBElementArray *search(NSArray *args)  {
+  NSString *query = [args componentsJoinedByString:@" "];
+  return [music searchFor:query only:iTunesESrAAll];
 }
 
-int main (int argc, const char * argv[])
-{
+void playTrackID(NSArray *args) {
+  NSString *databaseIdString = [args objectAtIndex:0];
+  NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
+  [f setNumberStyle:NSNumberFormatterDecimalStyle];
+  NSNumber *databaseId = [f numberFromString:databaseIdString];
+  [f release];
+  NSArray *matchingTracks = [[music tracks] filteredArrayUsingPredicate: [NSPredicate predicateWithFormat:@"databaseID == %@", databaseId]];
+  iTunesTrack* t = [matchingTracks objectAtIndex:0];
+  NSLog(@"playing track: %@", [t name]);
+  [t playOnce:false];
+}
 
+int main (int argc, const char * argv[]) {
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  NSArray *rawArgs = [[NSProcessInfo processInfo] arguments];
+  NSString *action;
+  NSArray *args;
 
-    // insert code here...
-    
-    NSString *query;
-    NSString *action; 
-    NSArray *args = [[NSProcessInfo processInfo] arguments];
-    if ([args count] > 2) {
-        action = [args objectAtIndex:1];
-        query = [args objectAtIndex:2];
-    } else {
-        action = @"search";
-        query = @"Bach";
-    };
-    
-    iTunesApplication *iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
-    library = [[iTunes sources] objectWithName:@"Library"];
-    music = [[library playlists] objectWithName:@"Music"];
-    if ([action isEqual: @"search"]) {
-      NSArray *tracks = search(query);
-      for (iTunesTrack *track in tracks) {
-        printf("%s\n", [format(track) cStringUsingEncoding: NSUTF8StringEncoding]);
-      }
-    } else if ([action isEqual: @"playID"]) { /* play by databaseId 
-                                                no need for search term
-    */
-      NSNumberFormatter * f = [[NSNumberFormatter alloc] init];
-      [f setNumberStyle:NSNumberFormatterDecimalStyle];
-      NSNumber *databaseId = [f numberFromString:[args objectAtIndex: 2]];
-      [f release];
-      NSLog(@"looking for track %@", databaseId);
-      playID(databaseId);
+  NSLog(@"args: %@", rawArgs);
+  if ([rawArgs count] < 2) {
+    action = @"search";
+    args = [NSArray arrayWithObject:@"bach"];
+  } else {
+    action = [rawArgs objectAtIndex:1];
+    NSRange aRange;
+    aRange.location = 2;
+    aRange.length = [rawArgs count] - 2;
+    args = [rawArgs subarrayWithRange:aRange];
+  }
 
+  iTunesApplication *iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+  library = [[iTunes sources] objectWithName:@"Library"];
+  music = [[library playlists] objectWithName:@"Music"];
 
-    } else  { /* play by index */
-      NSString *index = [args objectAtIndex: 3];
-      play(query, index);
+  if ([action isEqual: @"search"]) {
+    NSArray *tracks = search(args);
+    for (iTunesTrack *track in tracks) {
+      printf("%s\n", [formatTrackForDisplay(track) cStringUsingEncoding: NSUTF8StringEncoding]);
     }
+  } else if ([action isEqual: @"playTrackID"]) { 
+    playTrackID(args);
+  } 
 
-
-    [pool drain];
-    return 0;
+  [pool drain];
+  return 0;
 }
 
